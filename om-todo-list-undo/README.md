b2d90d6a2ad52404f311b8dd3f1fe5fb7ababcd9
Will Fleming <wfleming77@gmail.com>
2014-12-23 16:30:35 -0800

    Add todo list with undo example

---
+++ om-todo-list-undo/README.md
@@ -0,0 +1,15 @@
+# om-undo
+
+Example of buildin an undo feature with om.
+
+```
+lein cljsbuild auto
+lein ring server-headless
+```
+
+## License
+
+Copyright © 2014 William Fleming
+
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
---
+++ om-todo-list-undo/project.clj
@@ -0,0 +1,25 @@
+(defproject om-todo-list-undo "0.1.0"
+  :description "ClojureScript om undo web app"
+    :url "https://github.com/wtfleming/clojurescript-examples"
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [compojure "1.3.1"]
+                 [ring/ring-defaults "0.1.2"]
+                 [sablono "0.2.22"]
+                 [om "0.8.0-beta5"]
+                 [org.clojure/clojurescript "0.0-2505"]]
+  :ring {:handler om-todo-list-undo.core.handler/app}
+  :profiles
+  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                        [ring-mock "0.1.5"]]}}
+  :source-paths ["src/clj"]
+  :plugins [[lein-cljsbuild "1.0.3"]
+            [lein-ring "0.8.13"]]
+  :cljsbuild {:builds
+              [{:id "app"
+                :source-paths ["src/cljs"]
+                :compiler {:output-to "resources/public/js/app.js"
+                           :optimizations :whitespace
+                           :pretty-print true}}]})
+
+
