6f878f431a2cba405b6ae715199f776a19dda6f7
Will Fleming <wfleming77@gmail.com>
2015-11-18 14:01:34 -0800

    update dependencies

--- om-hello-world/project.clj
+++ om-hello-world/project.clj
@@ -2,19 +2,19 @@
   :description "ClojureScript om hello world web app"
     :url "https://github.com/wtfleming/clojurescript-examples"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [compojure "1.3.1"]
-                 [ring/ring-defaults "0.1.2"]
-                 [sablono "0.2.22"]
-                 [org.om/om "0.8.0"]
-                 [org.clojure/clojurescript "0.0-2511"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [compojure "1.4.0"]
+                 [ring/ring-defaults "0.1.5"]
+                 [sablono "0.4.0"]
+                 [org.omcljs/om "0.9.0"]
+                 [org.clojure/clojurescript "1.7.170"]]
   :ring {:handler om-hello-world.core.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
                         [ring-mock "0.1.5"]]}}
   :source-paths ["src/clj"]
-  :plugins [[lein-cljsbuild "1.0.3"]
-            [lein-ring "0.8.13"]]
+  :plugins [[lein-cljsbuild "1.1.1"]
+            [lein-ring "0.9.7"]]
   :cljsbuild {:builds
               [{:id "app"
                 :source-paths ["src/cljs"]
